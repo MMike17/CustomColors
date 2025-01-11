a492e3480f9c70c8686ca08cb06c01af1b518e95
MMike17 <matthews.r.mike@gmail.com>
2025-01-11 17:35:36 +0100

    Update background service to trigger repaint from popup

--- background.js
+++ background.js
@@ -1,5 +1,33 @@
+let currentTab;
+
 // Invoked when tabs are updated
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	if (tab.url && tab.url.includes("itch.io/dashboard"))
-		chrome.tabs.sendMessage(tabId, { type: "Repaint" });
+	currentTab = tab;
+	TriggerRepaint();
 });
+
+// Invoked when we change tab
+chrome.tabs.onActivated.addListener((activeInfo) => {
+	GetCurrentTab((tab) => currentTab = tab);
+});
+
+// Invoked when we receive a message
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (message.greeting = "trigger-repaint")
+		TriggerRepaint();
+
+	sendResponse();
+});
+
+function TriggerRepaint() {
+	console.log("Sending message");
+
+	if (currentTab && currentTab.url.includes("itch.io/dashboard"))
+		chrome.tabs.sendMessage(currentTab.id, { type: "Repaint" });
+}
+
+async function GetCurrentTab(onResult) {
+	let queryOptions = { active: true, currentWindow: true };
+	let [tab] = await chrome.tabs.query(queryOptions);
+	onResult(tab);
+}
